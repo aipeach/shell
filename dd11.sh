e94645762738c13affc013ae429e0c31e0abf1df
cola <44671411+aipeach@users.noreply.github.com>
2022-01-27 13:23:37 +0800

    test

--- dd11.sh
+++ dd11.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-apt-get update
-apt-get install curl -y
+apt update
+apt install curl -y
 curl -fLO https://raw.githubusercontent.com/bohanyang/debi/master/debi.sh && chmod a+rx debi.sh
 bash debi.sh --cdn --network-console --ethx --bbr --user root --password Abc123 --authorized-keys-url https://github.com/aipeach.keys --timezone Asia/Shanghai
 shutdown -r now
